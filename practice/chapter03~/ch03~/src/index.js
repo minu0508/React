ecd7703956f3f7fb443f58fc43e3a929a0632b80
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-15 23:57:12 +0900

    Add files via upload
    - Add chapter05

--- practice/chapter03~/ch03~/src/index.js
+++ practice/chapter03~/ch03~/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './chapter05/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -23,13 +24,24 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
  * [setInterval 함수 사용]
  *  : 개발자가 맨 뒤에 설장한 값(ms)마다 새롭게 clock Component를 root.div에 Rendering.
  */
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-}, 1000);
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// }, 1000);
+
+/**
+ * Chapter_05
+ * [댓글 컴포넌트 만들기]
+ */
+
+root.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
