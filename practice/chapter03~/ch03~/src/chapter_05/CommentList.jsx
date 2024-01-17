3dc1c1740dd1a86df812eb154793249e8013c829
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-17 23:50:01 +0900

    Add files via upload
    - Edit jsx file.  (Content: use props, object separation)

--- practice/chapter03~/ch03~/src/chapter_05/CommentList.jsx
+++ practice/chapter03~/ch03~/src/chapter_05/CommentList.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "홍길동",
+        comment: "안녕하세요, props를 사용합니다.",
+    },
+    {
+        name: "가나다",
+        comment: "리액트 배울 것들이 너무 많아요.",
+    },
+    {
+        name: "리액트",
+        comment: "언젠가는 능숙해지겠죠~"
+    },
+];
+
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}/>
+                );
+            })}
         </div>
     );
 }
