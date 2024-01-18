9774a812732d695cf2dba7353432c323da5eea0f
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-19 00:13:49 +0900

    Add files via upload

--- practice/chapter03~/ch03~/src/chapter_06/Notificationist.jsx
+++ practice/chapter03~/ch03~/src/chapter_06/Notificationist.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotifications = [
     {
+        id: 1,
         message: "안녕하세요, 리액트입니다.",
     },
-    {
+    {   
+        id: 2,
         message: "리액트는 재미는 있습니다.",
     },
     {
+        id: 3,
         message: "Chapter 06입니다.",
     },
 ];
@@ -34,8 +37,29 @@ class NotificationList extends React.Component {
                     notifications: notifications,
                 });
             } else {
-
+                this.setState({
+                    notifications: [],
+                });
+                clearInterval(timer);
             }
         }, 1000);
     }
+
+    render() {
+        return (
+            <div>
+                {this.state.notifications.map((notification) => {
+                    return (
+                        <Notification 
+                            key={notification.id}
+                            id={notification.id}
+                            message={notification.message} 
+                        />
+                    );
+                })}
+            </div>
+        );
+    }
 }
+
+export default NotificationList;
