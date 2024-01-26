b080f23f65094581403ce0cd2f9a247f2ee66dd8
Son MinWoo <97577022+minu0508@users.noreply.github.com>
2024-01-26 23:38:05 +0900

    Add files via upload
    - Add Chapter 16 (Topic: Mini Project)

---
+++ practice/mini-blog/src/component/ui/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    padding: 8px 16px;
+    font-size: 16px;
+    border-width: 1px;
+    border-radius: 8px;
+    cursor: pointer;
+    margin-bottom: 16px;
+`;
+
+function Button(props) {
+    const { title, onClick } = props;
+
+    return <StyledButton onClick={onClick}>{title || "Button"}</StyledButton>
+}
+
+export default Button;
